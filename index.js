6117cbeceac6c0d5e1aa9f2a53841a45d3559840
Justin Paine <xxdesmus@gmail.com>
2013-08-04 15:52:26 -0700

    Update index.js
    changed port to 8001, addez gzip and cache control (not working yet)

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 
 // Constants
-var DEFAULT_PORT = 8080;
+var DEFAULT_PORT = 8001;
 var PORT = process.env.PORT || DEFAULT_PORT;
+
 
 // App
 var app = express();
@@ -10,5 +11,12 @@ app.get('/', function (req, res) {
   res.send('Hello World\n');
 });
 
-app.listen(PORT)
+// GZIP and set cache-control headers of 1 day
+var oneDay = 86400000;
+app.use(express.compress());
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+app.listen(8001, "0.0.0.0");
+// app.listen(PORT)
+
 console.log('Running on http://localhost:' + PORT);
